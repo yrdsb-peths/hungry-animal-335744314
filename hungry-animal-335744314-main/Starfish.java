a27c810f870ec06091441dec5ff79f4e3bb27555
335744314 <99493761+335744314@users.noreply.github.com>
2022-04-29 10:07:11 -0400

    I needed

---
+++ hungry-animal-335744314-main/Starfish.java
@@ -0,0 +1,17 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Creates a starfish that moves
+ */
+public class Starfish extends Actor
+{
+    public Starfish()
+    {
+        super();
+    }
+    
+    public void act() 
+    {
+        move(1);
+    }    
+}
